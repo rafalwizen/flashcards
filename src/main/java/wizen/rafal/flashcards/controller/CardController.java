ece04c1d1a4a814028860cb9789d18de28914aae
Rafal Wizen <rafal.wizen@gmail.com>
2019-10-29 19:01:45 +0100

    added button to next card

--- src/main/java/wizen/rafal/flashcards/controller/CardController.java
+++ src/main/java/wizen/rafal/flashcards/controller/CardController.java
@@ -1,22 +1,29 @@
 package wizen.rafal.flashcards.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import wizen.rafal.flashcards.entity.Card;
 import wizen.rafal.flashcards.service.CardService;
 
 @Controller
+@RequestMapping("/test")
 public class CardController {
 
 	private CardService cardService;
 	
+	@Autowired
+	public CardController (CardService theCardService) {
+		cardService = theCardService;
+	}
+	
 	// expose "/randomFlashcard"
-	@GetMapping("/game")
+	@RequestMapping("/game")
 	public String randomFlashcard(Model theModel){
 		Card tempCard = cardService.randomFlashcard();
-		//theModel.addAttribute("randomCard", tempCard);
+		theModel.addAttribute("randomCard", tempCard);
 		return "flashcards";
 		}
 }
